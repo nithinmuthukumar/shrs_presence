d6b249e7ec31481291d4b19e6c2c6834661f6767
Nithin Muthukumar <kathrikai30201094@gmail.com>
2023-12-25 23:15:10 -0500

    Adding various hooks to show state

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 mod builtin;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{
+    path::PathBuf,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 use builtin::PresenceBuiltin;
 use discord_rich_presence::{
@@ -10,10 +13,15 @@ use shrs::{
     anyhow::{self, anyhow, Result},
     prelude::*,
 };
+//OPTIONS
+//Start timer, count number of commands, display mode, show shell repo
+//Current directory, number of shrs sessions (tracked in some global manner)
+
 pub struct PresenceState {
     pub client: DiscordIpcClient,
     pub start: i64,
     pub commands_used: i32,
+    pub working_dir: PathBuf,
 }
 impl PresenceState {
     pub fn new() -> Self {
@@ -24,6 +32,7 @@ impl PresenceState {
                 .unwrap()
                 .as_millis() as i64,
             commands_used: 0,
+            working_dir: PathBuf::new(),
         }
     }
     pub fn connect(&mut self) -> Result<()> {
@@ -32,12 +41,21 @@ impl PresenceState {
         }
         Ok(())
     }
-    pub fn update_activity(&mut self) {
+
+    pub fn update_activity(&mut self, state: &str) {
+        let home = std::env::var("HOME").unwrap();
+        let mut wd = self.working_dir.clone();
+
+        if let Ok(p) = wd.strip_prefix(home) {
+            wd = PathBuf::from("~").join(p);
+        }
+
+        let details = format!("📁 {}", wd.to_string_lossy());
         self.client
             .set_activity(
                 Activity::new()
-                    .state("1 session(s) open")
-                    .details(format!("{} commands used", self.commands_used).as_str())
+                    .state(state)
+                    .details(details.as_str())
                     .timestamps(Timestamps::new().start(self.start)),
             )
             .unwrap();
@@ -53,28 +71,59 @@ impl Plugin for PresencePlugin {
     fn init(&self, shell: &mut shrs::ShellConfig) -> anyhow::Result<()> {
         let mut state = PresenceState::new();
         state.connect()?;
-        state.update_activity();
 
         shell.builtins.insert("presence", PresenceBuiltin {});
         shell.state.insert(state);
-        shell.hooks.insert(set_activity_hook);
+        shell.hooks.insert(startup_hook);
+        shell.hooks.insert(begin_hook);
+        shell.hooks.insert(after_hook);
 
         Ok(())
     }
 }
-//Start timer, count number of commands, display mode, show shell repo
-//Current directory, number of shrs sessions (tracked in some global manner)
-fn set_activity_hook(
+fn after_hook(
     _sh: &Shell,
     sh_ctx: &mut Context,
-    _sh_rt: &mut Runtime,
+    sh_rt: &mut Runtime,
     ctx: &AfterCommandCtx,
 ) -> anyhow::Result<()> {
     if let Some(state) = sh_ctx.state.get_mut::<PresenceState>() {
         state.commands_used += 1;
-        sh_ctx.out.println(state.commands_used)?;
+        state.working_dir = sh_rt.working_dir.clone();
 
-        state.update_activity();
+        state.update_activity(
+            format!("Completed \"{}\" ({})", ctx.command, ctx.cmd_output.status).as_str(),
+        );
+    }
+
+    Ok(())
+}
+
+fn begin_hook(
+    _sh: &Shell,
+    sh_ctx: &mut Context,
+    sh_rt: &mut Runtime,
+    ctx: &BeforeCommandCtx,
+) -> anyhow::Result<()> {
+    if let Some(state) = sh_ctx.state.get_mut::<PresenceState>() {
+        state.commands_used += 1;
+        state.working_dir = sh_rt.working_dir.clone();
+
+        state.update_activity(format!("Running \"{}\"", ctx.command).as_str());
+    }
+
+    Ok(())
+}
+fn startup_hook(
+    _sh: &Shell,
+    sh_ctx: &mut Context,
+    sh_rt: &mut Runtime,
+    ctx: &StartupCtx,
+) -> anyhow::Result<()> {
+    if let Some(state) = sh_ctx.state.get_mut::<PresenceState>() {
+        state.commands_used += 1;
+        state.working_dir = sh_rt.working_dir.clone();
+        state.update_activity("Starting Shell");
     }
 
     Ok(())
