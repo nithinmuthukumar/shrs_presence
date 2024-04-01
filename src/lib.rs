4394295ced72c7837e0086ab0686ef1e9cdab3aa
Nithin Muthukumar <kathrikai30201094@gmail.com>
2024-04-01 18:29:01 -0400

    Adding launchd service

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,21 @@
 mod builtin;
 
-use std::time::Duration;
+use std::{
+    env::home_dir,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+    time::Duration,
+};
 
 use builtin::PresenceBuiltin;
 use reqwest::Client;
 use serde_json::json;
-use shrs::{anyhow::Result, prelude::*};
+use shrs::{
+    anyhow::{anyhow, Ok, Result},
+    prelude::*,
+};
+use std::process::Command;
 use uuid::Uuid;
 pub struct PresenceState {
     url: String,
@@ -24,7 +34,7 @@ impl PresenceState {
             let client = Client::new();
             loop {
                 tokio::time::sleep(Duration::from_secs(20)).await;
-                client.post(u.clone() + "/connect").json(&data).send().await;
+                let _ = client.post(u.clone() + "/connect").json(&data).send().await;
             }
         });
 
@@ -48,9 +58,57 @@ impl Default for PresencePlugin {
         Self::new("https://github.com/MrPicklePinosaur/shrs".to_string())
     }
 }
+fn setup_presence_daemon(config_dir: &PathBuf) -> Result<()> {
+    let cargo_dir = dirs::home_dir()
+        .ok_or(anyhow!("No Home Dir"))?
+        .join(".cargo/bin/shrs_presence_server");
+    let plist_file = config_dir.join("presence/com.nithin.shrs_presence_server.plist");
+    if let Some(parent) = plist_file.parent() {
+        fs::create_dir_all(parent)?;
+    }
+    if !plist_file.exists() {
+        let s = format!(
+            r#"<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+   <dict>
+       <key>Label</key>
+       <string>com.nithin.shrs_presence_server</string>
+       <key>Program</key>
+       <string>{}</string>
+       <key>RunAtLoad</key>
+       <true/>
+       <key>KeepAlive</key>
+       <true/>
+       <key>StandardOutPath</key>
+       <string>{}/presence/logfile.log</string>
+       <key>StandardErrorPath</key>
+       <string>{}/presence/error.log</string>
+   </dict>
+</plist>"#,
+            cargo_dir.to_string_lossy(),
+            config_dir.to_string_lossy(),
+            config_dir.to_string_lossy()
+        );
+        File::create(plist_file.clone())?.write_all(s.as_bytes())?;
+    }
+    dbg!(plist_file.clone());
+    let output = Command::new("launchctl")
+        .arg("load")
+        .arg(plist_file)
+        .output()?;
+
+    // Check if the job started successfully
+    if output.status.success() {
+        Ok(())
+    } else {
+        Err(anyhow!("launchctl failed"))
+    }
+}
 
 impl Plugin for PresencePlugin {
     fn init(&self, shell: &mut ShellConfig) -> anyhow::Result<()> {
+        setup_presence_daemon(&shell.config_dir)?;
         shell.builtins.insert("presence", PresenceBuiltin {});
         shell
             .state
@@ -59,6 +117,21 @@ impl Plugin for PresencePlugin {
         shell.hooks.insert(command_hook);
 
         Ok(())
+    }
+
+    fn meta(&self) -> PluginMeta {
+        PluginMeta::new(
+            "Presence",
+            "Plugin to enable discord presence",
+            Some("Please make sure the server is installed"),
+        )
+    }
+
+    fn fail_mode(&self) -> FailMode {
+        // Default to more strict fail mode to let users know faster there's a bug
+        //
+        // Should consider more how good of an idea this is
+        FailMode::Abort
     }
 }
 fn command_hook(
